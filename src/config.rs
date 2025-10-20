91da1e7e05655456db03fe8430fd25ff6413c756
Shenliehuozhi <5424462+shenliehuozhi@users.noreply.github.com>
2025-10-20 20:57:35 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -101,6 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["119.23.152.22"];
+
 pub const RS_PUB_KEY: &str = "gRBRxOBGTwiNg3JlhYecb2FcR+1Somp2G6mFfDZVkXs=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
